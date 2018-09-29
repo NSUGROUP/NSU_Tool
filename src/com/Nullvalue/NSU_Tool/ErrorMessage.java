4505f98eee7f26e2a2f10bb78d54b69e370a8a44
NullvaIue <tannerep@gmail.com>
2018-09-29 16:42:22 -0400

    Standalone error message handling
    Added error message handling so now errors display in a new JFrame window containing either stack traces or passed in Strings.

---
+++ src/com/Nullvalue/NSU_Tool/ErrorMessage.java
@@ -0,0 +1,64 @@
+package com.Nullvalue.NSU_Tool;
+
+import java.awt.Font;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ErrorMessage
+{
+	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    
+	int monitorWidth = gd.getDisplayMode().getWidth();
+    int monitorHeight = gd.getDisplayMode().getHeight();
+    
+    public void showMessage(String message)
+    {
+    	JFrame errorWindow = new JFrame();
+		
+		FontRenderContext frc = new FontRenderContext(new AffineTransform(), true, true);
+		Font font = new Font(Font.DIALOG, Font.PLAIN, 12);
+		int textWidth = 0;
+		
+		for (int i = 0; i < message.split("\n").length; i++)
+		{
+			if ((int) (font.getStringBounds(message.split("\n")[i], frc).getWidth()) > textWidth)
+				textWidth = (int) (font.getStringBounds(message.split("\n")[i], frc).getWidth());
+		}
+		
+		int windowWidthPadding = 60;
+		int windowHeightPadding = 100;
+		int windowWidth = textWidth + windowWidthPadding;
+		int windowHeight = (message.split("\n").length * 15) + windowHeightPadding;
+		
+		errorWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		errorWindow.setSize(windowWidth, windowHeight);
+		errorWindow.setLocation(((monitorWidth / 2) - windowWidth / 2), ((monitorHeight / 2) - windowHeight / 2));
+		
+		JLabel labelMessage = new JLabel("<HTML>" + message.replace("\n", "<br>") + "</HTML>", JLabel.CENTER);
+		labelMessage.setFont(font);
+		errorWindow.add(labelMessage);
+		
+		errorWindow.setVisible(true);
+    }
+    
+	public ErrorMessage(String message)
+	{
+		showMessage(message);
+	}
+	
+	public ErrorMessage(Exception e, String Precursor)
+	{
+		StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        String message = sw.toString();
+
+        showMessage("<b>" + Precursor + "</b>" + "\n" + message);
+	}
+}
--- src/com/Nullvalue/NSU_Tool/Main.java
+++ src/com/Nullvalue/NSU_Tool/Main.java
@@ -5,17 +5,11 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
-
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.WindowConstants;
 import javax.swing.JPanel;
-
-import org.zeroturnaround.zip.ZipUtil;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
@@ -28,53 +22,34 @@ public class Main
     public static interface User32 extends Library
     {
         User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);        
-        boolean SystemParametersInfo(int one, int two, String s, int three);         
+        boolean SystemParametersInfo(int one, int two, String s, int three);        
     }
     
     public static void main(String[] args)
     {
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-    	
-    	int montiorWidth = gd.getDisplayMode().getWidth();
+        
+    	int monitorWidth = gd.getDisplayMode().getWidth();
         int monitorHeight = gd.getDisplayMode().getHeight();
         int windowWidth = 350;
         int windowHeight = 200;
         
-        ImageIcon icon = new ImageIcon("src/b.png");
-        
     	window = new JFrame("NSU Tool");
     	JPanel mainPanel = new JPanel();
     	JButton bgButton = new JButton("Change Background");
-    	JButton zipButton = new JButton("Unzip File");
 
     	window.setSize(windowWidth, windowHeight);
-    	window.setIconImage(icon.getImage());
-    	window.setLocation(((montiorWidth / 2) - windowWidth / 2), ((monitorHeight / 2) - windowHeight / 2));
+    	window.setLocation(((monitorWidth / 2) - windowWidth / 2), ((monitorHeight / 2) - windowHeight / 2));
     	window.getContentPane().add(mainPanel);
-    	window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	
     	mainPanel.add(bgButton);
-    	mainPanel.add(zipButton);
     	
 		bgButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
 				changeBackground();
-			}
-		});
-		
-		zipButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0)
-			{
-				try
-				{
-					unzipFile();
-				} catch (Exception e)
-				{
-					e.printStackTrace();
-				}
 			}
 		});
     	
@@ -93,21 +68,6 @@ public class Main
             
             User32.INSTANCE.SystemParametersInfo(0x0014, 0, imagePath, 1);
             System.out.println(String.format("Changed desktop wallpaper to %s", imagePath));
-        }
-    }
-    
-    public static void unzipFile()
-    {
-    	JFileChooser zipSelector = new JFileChooser();
-        int returnValue = zipSelector.showOpenDialog(window);
-        
-        if (returnValue == JFileChooser.APPROVE_OPTION)
-        {
-        	String zipPath = zipSelector.getSelectedFile().getPath();
-        	String zipName = zipSelector.getSelectedFile().getName().replaceAll("[.]", "");
-        	String output  = zipSelector.getSelectedFile().getParent() + "\\" + zipName;
-        	
-        	ZipUtil.unpack(new File(zipPath), new File(output));
         }
     }
 
@@ -131,13 +91,10 @@ public class Main
                     Runtime.getRuntime().exec(ExePath);
                     System.out.println(String.format("Attempted to open program at path %s", ExePath));
                     
+                } catch (IOException openFileException) {
+                    new ErrorMessage(openFileException, "Error occured while opening file...");
                 }
-                catch (IOException e)
-                {
-                    System.err.println("Error occured while opening file...");
-                    e.printStackTrace();
-                }
-            }else{
+            } else {
                 System.out.println(String.format("Failed to open file %s", ExePath));
             }
         }
