67cba25057fd8b6f560d77245965bef3757f6270
NullvaIue <tannerep@gmail.com>
2018-09-29 17:28:11 -0400

    File extension checks
    Added both file extension filters and checks that inhibit you if you don't choose a supported file type for the operation.

--- src/com/Nullvalue/NSU_Tool/Main.java
+++ src/com/Nullvalue/NSU_Tool/Main.java
@@ -5,11 +5,13 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
@@ -31,19 +33,23 @@ public class Main
         
     	int monitorWidth = gd.getDisplayMode().getWidth();
         int monitorHeight = gd.getDisplayMode().getHeight();
-        int windowWidth = 350;
+        int windowWidth = 220;
         int windowHeight = 200;
         
     	window = new JFrame("NSU Tool");
     	JPanel mainPanel = new JPanel();
-    	JButton bgButton = new JButton("Change Background");
 
     	window.setSize(windowWidth, windowHeight);
     	window.setLocation(((monitorWidth / 2) - windowWidth / 2), ((monitorHeight / 2) - windowHeight / 2));
     	window.getContentPane().add(mainPanel);
     	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	window.setResizable(false);
+
+    	JButton bgButton = new JButton("Change Background");
+    	JButton exeButton = new JButton("Executable");
     	
     	mainPanel.add(bgButton);
+    	mainPanel.add(exeButton);
     	
 		bgButton.addActionListener(new ActionListener()
 		{
@@ -52,39 +58,59 @@ public class Main
 				changeBackground();
 			}
 		});
+		
+		exeButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				openExe();
+			}
+		});
     	
     	window.setVisible(true);
     }
     
     public static void changeBackground()
     {
-    	JFileChooser imageSelector = new JFileChooser();
+    	JFileChooser imageSelector = new JFileChooser(System.getProperty("user.home") + "/Desktop/");
+    	FileNameExtensionFilter filter = new FileNameExtensionFilter("png/jpeg/gif", "jpg", "png", "JPEG", "gif");
+    	imageSelector.setFileFilter(filter);
+    	
         int returnValue = imageSelector.showOpenDialog(window);
         String imagePath;
         
         if (returnValue == JFileChooser.APPROVE_OPTION)
         {
-            imagePath = imageSelector.getSelectedFile().getPath();
-            
-            User32.INSTANCE.SystemParametersInfo(0x0014, 0, imagePath, 1);
-            System.out.println(String.format("Changed desktop wallpaper to %s", imagePath));
+        	System.out.println(getFileExtension(imageSelector.getSelectedFile()));
+        	if (getFileExtension(imageSelector.getSelectedFile()).equals("jpg") || getFileExtension(imageSelector.getSelectedFile()).equals("png") || getFileExtension(imageSelector.getSelectedFile()).equals("gif"))
+        	{
+	            imagePath = imageSelector.getSelectedFile().getPath();
+	            
+	            User32.INSTANCE.SystemParametersInfo(0x0014, 0, imagePath, 1);
+	            System.out.println(String.format("Changed desktop wallpaper to %s", imagePath));
+        	} else {
+        		new ErrorMessage("You can only select files with the extension jpg, png, or gif.");
+        	}
         }
     }
 
     public static void openExe()
-    {       
+    {
         Frame mainFrame = new Frame("Pick Frame");
         mainFrame.setSize(0, 0);
         String ExePath = null;
         
-        JFileChooser exeFile = new JFileChooser();
+        JFileChooser exeFile = new JFileChooser(System.getProperty("user.home") + "/Desktop/");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("exe", "exe", "Executable");
+        exeFile.setFileFilter(filter);
+        
         int returnValue = exeFile.showOpenDialog(mainFrame);
         
         if (returnValue == JFileChooser.APPROVE_OPTION)
         {
-            if (exeFile.getSelectedFile().canExecute() == true)
-            {
-                ExePath = exeFile.getSelectedFile().getPath();
+        	if (getFileExtension(exeFile.getSelectedFile()).equals("exe"))
+        	{
+        		ExePath = exeFile.getSelectedFile().getPath();
                 
                 try
                 {
@@ -94,9 +120,26 @@ public class Main
                 } catch (IOException openFileException) {
                     new ErrorMessage(openFileException, "Error occured while opening file...");
                 }
-            } else {
-                System.out.println(String.format("Failed to open file %s", ExePath));
-            }
+        	} else {
+        		new ErrorMessage("You must choose an executable file (.exe)");
+        	}
         }
+    }
+    
+    private static String getFileExtension(File file)
+    {
+        String extension = "";
+ 
+        try
+        {
+            if (file != null && file.exists())
+            {
+                extension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+            }
+        } catch (Exception e) {
+            extension = "";
+        }
+ 
+        return extension;
     }
 }
