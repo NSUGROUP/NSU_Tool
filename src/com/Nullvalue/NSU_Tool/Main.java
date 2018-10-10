da628a680da30e44f9881462b54d684b2067aa1a
NullvaIue <tannerep@gmail.com>
2018-10-10 19:42:15 -0400

    Chromium
    Add the ability for the user to install a Chromium distribution with built in vpn support.

--- src/com/Nullvalue/NSU_Tool/Main.java
+++ src/com/Nullvalue/NSU_Tool/Main.java
@@ -7,10 +7,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -60,12 +67,14 @@ public class Main
 
     	JButton bgButton = new JButton("Change Background");
     	JButton exeButton = new JButton("Add Executable");
+    	JButton chromiumButton = new JButton("Install Chromium");
     	
-    	JCheckBox nearbyFilesCheck = new JCheckBox("<html>Include files in parent<br>directory as dependencies</html>");
+    	JCheckBox nearbyFilesCheck = new JCheckBox("<html>Include files in parent<br>directory as dependencies<br></html>");
     	
     	mainPanel.add(bgButton);
     	mainPanel.add(exeButton);
     	mainPanel.add(nearbyFilesCheck);
+    	mainPanel.add(chromiumButton);
     	
 		bgButton.addActionListener(new ActionListener()
 		{
@@ -90,6 +99,14 @@ public class Main
 	        	nearbyCheckState = nearbyFilesCheck.isSelected();
 		    }
 		});
+		
+		chromiumButton.addActionListener(new ActionListener()
+		{
+		    public void actionPerformed(ActionEvent e)
+		    {
+	        	installChromium();
+		    }
+		});
     	
     	window.setVisible(true);
     }
@@ -97,7 +114,7 @@ public class Main
     public static void changeBackground()
     {
     	JFileChooser imageSelector = new JFileChooser(System.getProperty("user.home") + "/Desktop/");
-    	FileNameExtensionFilter filter = new FileNameExtensionFilter("png/jpeg/gif", "jpg", "png", "JPEG", "gif");
+    	FileNameExtensionFilter filter = new FileNameExtensionFilter("Images (.jpg, .png, .gif)", "jpg", "png", "JPEG", "gif");
     	imageSelector.setFileFilter(filter);
     	
         int returnValue = imageSelector.showOpenDialog(window);
@@ -126,7 +143,7 @@ public class Main
 	        mainFrame.setSize(0, 0);
 	        
 	        JFileChooser exeFile = new JFileChooser(System.getProperty("user.home") + "/Desktop/");
-	        FileNameExtensionFilter filter = new FileNameExtensionFilter("exe", "exe", "Executable");
+	        FileNameExtensionFilter filter = new FileNameExtensionFilter("Executable (.exe)", "exe", "Executable");
 	        exeFile.setFileFilter(filter);
 	        
 	        int returnValue = exeFile.showOpenDialog(mainFrame);
@@ -214,6 +231,67 @@ public class Main
 	        }
     	} else {
     		new ErrorMessage("This tool must be run on an OCPS distributed machine to function.");
+    	}
+    }
+    
+    public static void installChromium()
+    {
+    	if (new File(System.getenv("ProgramFiles(X86)") + "\\DRC INSIGHT Online Assessments").exists() && !(new File(System.getenv("ProgramFiles(X86)") + "\\DRC INSIGHT Online Assessments\\Chromium-70.0.3538.9").exists()))
+    	{
+    		String drcFolder = System.getenv("ProgramFiles(X86)") + "\\DRC INSIGHT Online Assessments\\";
+    		
+    		try
+			{
+    			FileUtils.copyURLToFile(new URL("https://github.com/free-vpn/chrome/releases/download/70.0.3538.9/Chromium-Windows-70.0.3538.9.zip"), new File(drcFolder + "Chromium VPN.zip"), 30000, 5000000);
+				
+				int BUFFER = 2048;
+		        File file = new File(drcFolder + "Chromium VPN.zip");
+
+		        @SuppressWarnings("resource")
+				ZipFile zip = new ZipFile(file);
+		        String newPath = System.getenv("ProgramFiles(X86)") + "\\DRC INSIGHT Online Assessments\\";
+
+		        new File(newPath).mkdir();
+		        Enumeration<? extends ZipEntry> zipFileEntries = zip.entries();
+
+		        while (zipFileEntries.hasMoreElements())
+		        {
+		            ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
+		            String currentEntry = entry.getName();
+
+		            File destFile = new File(newPath, currentEntry);
+		            File destinationParent = destFile.getParentFile();
+
+		            destinationParent.mkdirs();
+
+		            if (!entry.isDirectory())
+		            {
+		                BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
+		                int currentByte;
+		                byte data[] = new byte[BUFFER];
+
+		                FileOutputStream fos = new FileOutputStream(destFile);
+		                BufferedOutputStream dest = new BufferedOutputStream(fos,
+		                BUFFER);
+
+		                while ((currentByte = is.read(data, 0, BUFFER)) != -1)
+		                {
+		                    dest.write(data, 0, currentByte);
+		                }
+		                
+		                dest.flush();
+		                dest.close();
+		                is.close();
+		            }
+		        }
+		        
+		        ShellLink.createLink(new File(drcFolder + "Chromium-70.0.3538.9\\chrome.exe").getPath(), System.getProperty("user.home") + "/Desktop/Chromium.lnk");
+		        new ErrorMessage("Chromium successfully installed, shortcut added to the desktop.");
+			} catch (Exception e) {
+				new ErrorMessage(e);
+			}
+    	} else {
+    		new ErrorMessage("This tool must be run on an OCPS distributed machine to function or Chromium is already installed.");
     	}
     }
     
